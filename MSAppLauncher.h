284c6c68bcc77811aaede7a7497d7556987c9939
matt.stoker <matt.stoker@eff6c5b1-8f37-0410-859f-a9980883110d>
2007-08-17 21:03:59 +0000

    Added app launcher class.  Doesn't compile!

---
+++ MSAppLauncher.h
@@ -0,0 +1,36 @@
+/*
+	MSAppLauncher.h
+	
+	MobileStudio standard application launcher
+	
+	Copyright 2007 Matt Stoker
+	Begun: Aug/17/2007
+	
+	Thanks: iPhone Dev Team for Compilation Toolchain
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; version 2
+	of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIApplication.h>
+
+@interface MSAppLauncher
+{
+}
+
++ (void) launchApplication: (NSString*)appID withApplication: (UIApplication*)app;
++ (void) launchApplication: (NSString*)appID withAppBundlePath: (NSString*)appBundlePath withArguments: (NSArray*) args withApplication: (UIApplication*)app withLaunchingAppID: (NSString*)launchingAppID withLaunchingAppBundlePath: (NSString*)launchingAppBundlePath;
+@end
---
+++ MSAppLauncher.m
@@ -0,0 +1,63 @@
+/*
+	MSAppLauncher.m
+	
+	MobileStudio standard application launcher
+	
+	Copyright 2007 Matt Stoker
+	Begun: Aug/17/2007
+	
+	Thanks: iPhone Dev Team for Compilation Toolchain
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; version 2
+	of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIApplication.h>
+#import "MSAppLauncher.h"
+
+@implementation MSAppLauncher
+
++ (void) launchApplication: (NSString*)appID withApplication: (UIApplication*)app
+{
+	//Regular app launch method: launch with no LaunchInfo.plist (Thanks Launcher.app dev team!)
+	[app launchApplicationWithIdentifier: appID suspended: NO];
+}
+
++ (void) launchApplication: (NSString*)appID withAppBundlePath: (NSString*)appBundlePath withArguments: (NSArray*) args withApplication: (UIApplication*)app withLaunchingAppID: (NSString*)launchingAppID withLaunchingAppBundlePath: (NSString*)launchingAppBundlePath
+{
+	//Build LaunchInfo.plist dictionary
+	NSDictionary* plist = [[NSDictionary alloc] initWithObjectsAndKeys:
+		launchingAppID, @"MSLaunchingAppIdentifier",
+		launchingAppPath, @"MSLaunchingAppBundlePath",
+		appID, @"MSLaunchedAppIdentifier",
+		appBundlePath, @"MSLaunchedAppBundlePath",		
+		args, @"MSLaunchedAppArgs"];
+	
+	//Seralize LaunchInfo.plist dictionary
+	dataFromPropertyList:(id)plist format:(NSPropertyListFormat)format errorDescription:(NSString **)errorString;
+	NSData* rawPList = [NSPropertyListSerialization dataFromPropertyList: plist		
+		format:
+		errorDescription:
+		error:];
+	
+	//Write LaunchInfo.plist file
+	[rawPList writeFile
+
+	//Actually launch application
+	[MSAppLauncher launchApplication: appID withApplication: app];
+}
+
+@end
